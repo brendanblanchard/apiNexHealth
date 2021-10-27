983af60acab8c2c975264ea1db90e0d4106bba26
Brendan-Blancha <>
2021-10-27 13:51:33 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var iframe = document.getElementById("iFrameResizer1");
+
+var height = iframe.height;
+
+var selected = document.getElementsByClassName({checked: category.category === intake.appt_category});
+
+console.log("selected item intake.appt");
